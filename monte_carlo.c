9620ba9d2501b2f0f251995509fece03f280f00d
blazinflame <84576462+blazinflame@users.noreply.github.com>
2021-05-31 11:17:54 +0530

    Update monte_carlo.c
    function definition for monte carlo
    using frandom 
    using sqrt function to find the radius of circle
    checking whether points a and b are inside the circle
    adding up the number of points inside the circle

--- monte_carlo.c
+++ monte_carlo.c
@@ -3,15 +3,37 @@
 #include <stdio.h>
 #include <math.h>
 
-float mc_pi(int);
-
-float frandom() {
+float frandom() 
+{
   long int q = random();
   float ret = (float)q/(float)RAND_MAX;
   return ret;
 }
 
-int main(void) {
+float mc_pi(int points)
+{
+  int i;
+  float number=0.0;
+  float a,b,r,pivalue;
+  for (i = 0; i <= points; i++)
+  {
+    a = frandom();
+    b = frandom();
+    r = sqrt ((a * a) + (b * b));
+    if(r < 1)
+    {
+      number++;
+    }
+  }
+    
+    pivalue = ((4 * number) / points);
+      return pivalue;
+}
+
+
+
+int main(void)
+{
   float pi0;
   float pi1;
   
@@ -19,20 +41,24 @@ int main(void) {
   pi1 = mc_pi(25000);
   printf("%f %f\n", pi0, pi1);
   
-  if (pi0 == pi1) {
+  if (pi0 == pi1) 
+  {
       printf("Two separate estimates of pi are exactly the same. This is unlikely.\n");
       abort();
     }
 
-  if (fabs(pi0 - pi1) > 0.05) {
+  if (fabs(pi0 - pi1) > 0.05)
+  {
       printf("Two separate estimates %f and %f are too different.\n", pi0, pi1);
       abort();
   }
 
     
-  for (int i=2000; i<5000; i++) {
+  for (int i=2000; i<5000; i++)
+  {
     pi0 = mc_pi(i);
-    if (!(fabs(pi0 - M_PI) < 0.4)) {
+    if (!(fabs(pi0 - M_PI) < 0.4))
+    {
       printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi0);
       abort();
     }
