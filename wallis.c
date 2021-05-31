33fcb3e7a3b4c96eb5047716ff8a2f63fd69f957
blazinflame <84576462+blazinflame@users.noreply.github.com>
2021-05-31 11:02:08 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -8,7 +8,7 @@ float wallis_pi(int limit)
   float square, number; 
   float pi = 1.0;
   int i;
-  for(i = 1; i <= limit; i++)
+  for(i = 0; i <= limit; i++)
   {
    
     square = i * i;
@@ -18,20 +18,25 @@ float wallis_pi(int limit)
   return pi * 2;
 }
 
-int main(void) {
+int main(void)
+{
   float pi;
-  for (int i=0; i<5; i++) {
+  for (int i=0; i<5; i++)
+  {
     pi = wallis_pi(i);
     //function call
-    if (!(fabs(pi - M_PI) > 0.15)) {
+    if (!(fabs(pi - M_PI) > 0.15))
+    {
       printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
       abort();
     }
   }
 
-  for (int i=500; i<3000; i++) {
+  for (int i=500; i<3000; i++) 
+  {
     pi = wallis_pi(i);
-    if (!(fabs(pi - M_PI) < 0.01)) {
+    if (!(fabs(pi - M_PI) < 0.01)) 
+    {
       printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
       abort();
     }
